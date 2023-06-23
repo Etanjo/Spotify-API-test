e281500699b3ddb79ac2fbfd68e50162c8ac1a9f
Etanjo <99751108+Etanjo@users.noreply.github.com>
2023-06-23 15:31:31 +0000

    created project

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
-<script>
+<script lang="ts">
   import Hello from "./Hello.svelte";
+  let greeting: string = "Hello";
 </script>
 
 <main>
   <!-- Replace this with your actual content to make a project :) -->
   <Hello />
+  <h1>{greeting} World!</h1>
 </main>
 
 <style>
   main {
-    
   }
-  
 </style>
